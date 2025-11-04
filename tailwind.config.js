d4e6366d4901e8a4498eea61db743b57c6e2c6b7
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-11-05 03:16:11 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+        montserrat: ["Montserrat", "system-ui"]
+      }
+    }
+  },
+  plugins: []
+};
